2e9122bd2b36d9d7c9b5ab8f383c799897d66ab7
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2026-02-11 15:52:13 +0100

    Solve prop drilling with component composition

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,13 @@ import { useState } from 'react';
 import { tempMovieData, tempWatchedData } from '../data-templates';
 import Nav from './Nav';
 import Main from './Main';
-import SearchResults from './SearchResults';
-import WatchedMovies from './WatchedMovies';
+import MovieList from './MovieList';
+import WatchedSummary from './WatchedSummary';
+import WatchedMovieList from './WatchedMovieList';
+import Box from './Box';
+import Logo from './Logo';
+import Search from './Search';
+import NumResults from './NumResults';
 
 function App() {
   const [query, setQuery] = useState('');
@@ -12,11 +17,21 @@ function App() {
 
   return (
     <>
-      <Nav query={query} onSetQuery={setQuery} movies={movies} />
+      <Nav>
+        <Logo />
+        <Search query={query} onSetQuery={setQuery} />
+        <NumResults movies={movies} />
+      </Nav>
 
       <Main>
-        <SearchResults movies={movies} />
-        <WatchedMovies watched={watched} />
+        <Box>
+          <MovieList movies={movies} />
+        </Box>
+
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMovieList watched={watched} />
+        </Box>
       </Main>
     </>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,40 +1,5 @@
-function Nav({ query, onSetQuery, movies }) {
-  return (
-    <nav className='nav-bar'>
-      <Logo />
-      <Search query={query} onSetQuery={onSetQuery} />
-      <NumResults movies={movies} />
-    </nav>
-  );
-}
-
-function Logo() {
-  return (
-    <div className='logo'>
-      <span role='img'>🍿</span>
-      <h1>usePopcorn</h1>
-    </div>
-  );
-}
-
-function Search({ query, onSetQuery }) {
-  return (
-    <input
-      className='search'
-      type='text'
-      placeholder='Search movies...'
-      value={query}
-      onChange={e => onSetQuery(e.target.value)}
-    />
-  );
-}
-
-function NumResults({ movies }) {
-  return (
-    <p className='num-results'>
-      Found <strong>{movies.length}</strong> results
-    </p>
-  );
+function Nav({ children }) {
+  return <nav className='nav-bar'>{children}</nav>;
 }
 
 export default Nav;
