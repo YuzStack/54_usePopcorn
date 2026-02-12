59c6bf381d717389e40e58a8cf557e337da3efff
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2026-02-12 06:41:44 +0100

    Build a reusable rating component separately

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react';
+import { StrictMode, useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './components/App';
+import StarRating from './components/StarRating';
+// import './index.css';
+// import App from './components/App';
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color='blue' maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars.</p>
+    </div>
+  );
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+      defaultRating={3}
+    />
+    <StarRating
+      maxRating={10}
+      size={25}
+      color='red'
+      className='test'
+      defaultRating={5}
+    />
+
+    <Test />
   </StrictMode>,
 );
