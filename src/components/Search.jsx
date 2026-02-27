76ac49a1ab8dff77c4c993dc8d4e6f6474c8bc13
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2026-02-27 14:05:27 +0100

    use the 'useRef' hook to implement autofocus feature

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,35 @@
+import { useEffect, useRef } from 'react';
+
 function Search({ query, onSetQuery }) {
+  const inputRef = useRef(null);
+
+  useEffect(
+    function () {
+      const callback = function (e) {
+        if (e.key === 'Enter' && inputRef.current !== document.activeElement) {
+          onSetQuery('');
+          inputRef.current.focus();
+        }
+      };
+
+      document.addEventListener('keydown', callback);
+
+      return function () {
+        document.removeEventListener('keydown', callback);
+      };
+    },
+    [onSetQuery],
+  );
+
   return (
     <input
       className='search'
       type='text'
       placeholder='Search movies...'
       value={query}
+      autoFocus
       onChange={e => onSetQuery(e.target.value)}
+      ref={inputRef}
     />
   );
 }
